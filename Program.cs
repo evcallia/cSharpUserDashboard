23a2371314124ccb0fba9cef389704e1dc1b6921
Evan <calliaevan12@gmail.com>
2016-10-18 14:45:15 -0700

    finished deployment settings

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace userDashboard
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
+                .UseIISIntegration()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
